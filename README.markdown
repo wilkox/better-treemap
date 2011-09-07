338e9dec60ad75b912dc87d0519ce4ea38174186
David Wilkins <david@wilkox.org>
2011-09-07 15:25:27 +1000

    Added README and license details

---
+++ README.markdown
@@ -0,0 +1,2 @@
+#Better Treemap
+An R function to draw treemaps, nicely. Forked from `map.market` from the [portfolio package](http://cran.r-project.org/web/packages/portfolio/index.html) by Jeff Enos and David Kane, with contributions from Daniel Gerlanc and Kyle Campbell, and accordingly released under [GPL V3](http://www.gnu.org/licenses/gpl.html).
--- better-treemap.R
+++ better-treemap.R
@@ -1,3 +1,24 @@
+#Derived from map.market from the portfolio package, by 
+# Jeff Enos and David Kane, with contributions from 
+# Daniel Gerlanc and Kyle Campbell
+# http://cran.r-project.org/web/packages/portfolio/index.html
+
+# better-treemap is by David Wilkins <david@wilkox.org>
+# it lives at https://github.com/wilkox/better-treemap
+
+#This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 better.treemap <- function(id, area, group, color,
                        scale = NULL,
                        lab   = c(TRUE, FALSE),
